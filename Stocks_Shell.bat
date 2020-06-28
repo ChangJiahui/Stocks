5a1881e1f63fcdefa3ed44583b199fae8780b728
keywater <changjh13@mails.tsinghua.edu.cn>
2020-06-28 15:56:30 +0800

    Update Stocks_Shell.bat
    Add Bonddata Analyze

--- Stocks_Shell.bat
+++ Stocks_Shell.bat
@@ -11,6 +11,12 @@ C:\Users\CHANG\AppData\Local\conda\conda\envs\Stocks\python.exe D:\Workspace\Pyt
 echo %Date% %Time%: fundsAnalyze.py Program Finished! >> D:\Workspace\Python\Stocks\Logger.log
 echo %Date% %Time%: fundsAnalyze.py Program Finished! >> D:\Workspace\Python\Stocks\Error.log
 TIMEOUT /T 10 /NOBREAK
+echo %Date% %Time%: bondsAnalyze.py Program Run! >> D:\Workspace\Python\Stocks\Logger.log
+echo %Date% %Time%: bondsAnalyze.py Program Run! >> D:\Workspace\Python\Stocks\Error.log
+C:\Users\CHANG\AppData\Local\conda\conda\envs\Stocks\python.exe D:\Workspace\Python\Stocks\Code\bondsAnalyze.py 1>>D:\Workspace\Python\Stocks\Logger.log 2>>D:\Workspace\Python\Stocks\Error.log
+echo %Date% %Time%: bondsAnalyze.py Program Finished! >> D:\Workspace\Python\Stocks\Logger.log
+echo %Date% %Time%: bondsAnalyze.py Program Finished! >> D:\Workspace\Python\Stocks\Error.log
+TIMEOUT /T 10 /NOBREAK
 echo %Date% %Time%: stocksTrade.py Program Run! >> D:\Workspace\Python\Stocks\Logger.log
 echo %Date% %Time%: stocksTrade.py Program Run! >> D:\Workspace\Python\Stocks\Error.log
 C:\Users\CHANG\AppData\Local\conda\conda\envs\Stocks\python.exe D:\Workspace\Python\Stocks\Code\stocksTrade.py 1>>D:\Workspace\Python\Stocks\Logger.log 2>>D:\Workspace\Python\Stocks\Error.log
