33c2ce918acdc8654ce87eb499e37d9b0165e306
ChangJiahui <changjh17@mails.tsinghua.edu.cn>
2020-08-31 17:32:54 +0800

    Update .bat
    Change Env Path

--- StocksRealtime.bat
+++ StocksRealtime.bat
@@ -1,1 +1,6 @@
-C:\Users\CHANG\AppData\Local\conda\conda\envs\Stocks\python.exe D:\Workspace\Python\Stocks\Code\stocksRealtime.py
+@echo off
+echo %Date% %Time% stocksRealtime.py Program Run! >> D:\Workspace\Python\Stocks\Logger.log
+echo %Date% %Time% stocksRealtime.py Program Run! >> D:\Workspace\Python\Stocks\Error.log
+C:\Users\Chang\.conda\envs\Stocks\python.exe D:\Workspace\Python\Stocks\Code\stocksRealtime.py
+echo %Date% %Time% stocksRealtime.py Program Finished! >> D:\Workspace\Python\Stocks\Logger.log
+echo %Date% %Time% stocksRealtime.py Program Finished! >> D:\Workspace\Python\Stocks\Error.log
